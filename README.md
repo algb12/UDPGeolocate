9b0d85a54c630d19ddb04bea9161649d201fdfae
Alexander Gilburg (algb12) <alexander.gilburg@gmail.com>
2017-08-22 19:34:52 +0200

    added Windows support

--- README.md
+++ README.md
@@ -17,24 +17,46 @@ Also, the country/city of the stranger may provide for some good conversation st
 
 Basically, use with care! I am NOT responsible or liable in any way for any direct or indirect damages incurred by this script, just saying!
 
-## How to use UDPGeolocate
-Make sure that you've got Python 2.x installed on your OS, download this repository, and then run from the terminal with root privileges (using `sudo`, a root shell or similar mechanisms): `python /path/to/UDPGeolocate.py`
+## Prerequisites
+### Installing Python 2.x (if applicable)
+Make sure that you've got the latest Python 2.x installed on your OS. On OS X and Linux, this should be available out-of-the-box. If, for some strange reason, it isn't, it can be installed too. On Windows, however, it has to be installed. When installing Python on Windows, **make sure to enable the "Add to Path" option in the Python installer**!
 
-Make sure to replace the path with the correct path to the script.
+Python can be downloaded from the [official Python website](https://www.python.org/).
+
+### Installing WinPcap or Win10Pcap (Windows only)
+For packet-layer network traffic access, and for WinDump to work, Windows needs the WinPcap drivers, which can be [downloaded here](https://www.winpcap.org/).
+
+From Windows 10 onwards, due to a different Network Driver Interface Specification (NDIS), Win10Pcap has to be installed instead, which can be [downloaded here](http://www.win10pcap.org). WinDump will also work with Win10Pcap, according to [this source](https://www.coursehero.com/file/p79k3e2/Installing-Windump-Install-the-Windows-10-WinPcap-library-from/).
+
+### Downloading UDPGeolocate
+Obviously, this script has to be downloaded in order to be used. Simply download a .ZIP of this repository, extract it, and you're good to go!
+
+### Downloading WinDump
+As a tcpdump drop-in replacement, WinDump is the sniffer used by the script on Windows. The WinDump executable can be [downloaded here](https://www.winpcap.org/windump/), and should be placed in the same directory as the UDPGeolocate.py file resides in.
+
+Normally, downloading WinDump is **not necessary**, because UDPGeolocate tries to download WinDump by itself if it is missing from its directory. If the download fails, however, UDPGeolocate will instruct the user to download WinDump and place it in the script directory at root level.
+
+## Running UDPGeolocate
+Make sure to replace the example path with the correct path to the script.
+### OS X/Linux
+Run from the terminal with root privileges (using `sudo`, a root shell or similar mechanisms): `python /path/to/UDPGeolocate.py`
+
+### Windows
+Run from the command line: `python.exe \path\to\UDPGeolocate.py`
+
+In case the prerequisites check fails, follow the instructions printed out by UDPGeolocate.
 
 ## Config
 ### Minimum packet length
 This is the minimum packet length (including things besides the payload, such as the header and IP information) which should trigger UDPGeolocate's querying mechanism. Setting it to low can cause rogue UDP packets to trigger false results with UDPGeolocate.
 
 ### UDP port
-The port on which UDPGeolocate should listen on for packets. Can be detected by UDPGeolocate by running the video chat service with microphone and camera disabled. Stranger must have video enabled for detection to work. A skip or two shouldn't throw the detection off too much.
+The port on which UDPGeolocate should listen on for packets. Can be detected by UDPGeolocate by running the video chat service with microphone and camera disabled. Stranger must have video enabled for detection to work. A skip or two shouldn't throw the detection off too much. After the port has been detected, you need to enable camera and microphone again for the geolocation to work properly.
 
 ### Minimum timeout
 The minimum amount of time that UDPGeolocate should take between checking on the IP address. Set to a higher value for less frequent checks, and for less strain on the CPU.
 
 ## Contributions
-If anybody has access to a windows machine, then I would gladly welcome any contributions to make this script work on windows! I was thinking of maybe using `netsh trace` or WinDump instead of tcpdump, so those may be viable solutions to get the script to work on Windows too. Sadly, I don't have access to a Windows development environment.
-
 For debugging, just set the logger level to logging.DEBUG in the script.
 
 In case of bugs, please open up an issue in the issue tracker, or email me: <algb12.19@gmail.com>
--- UDPGeolocate.py
+++ UDPGeolocate.py
@@ -5,21 +5,47 @@ logging.basicConfig(level=logging.INFO)
 
 # Constants
 GEOLOCATION_API_URL = 'http://ip-api.com/json/'
-devnull = open(os.devnull, 'wb')
+WINDUMP_URL = 'https://www.winpcap.org/windump/install/bin/windump_3_9_5/WinDump.exe'
+PORT_PROBING_TIME = 5
+NULL_DEVICE = open(os.devnull, 'wb')
+# Directory wherein this file is contained
+CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# Windows prerequisites check
+def Windows_prereq_check():
+    if platform.system() == 'Windows':
+        # Do we have WinPcap installed?
+        if os.path.isfile('C:\\Windows\System32\wpcap.dll') == False:
+            logging.error('WinPcap is not instaslled. Please install WinPcap.')
+            sys.exit(1)
+        # Is WinDump in the directory of this file?
+        if os.path.isfile(CUR_DIR + '\WinDump.exe') == False:
+            logging.warning('WinDump not found. Trying to download WinDump...')
+            try:
+                urllib.urlretrieve(WINDUMP_URL, CUR_DIR + "\WinDump.exe")
+            except:
+                logging.error('Couldn\'t download WinDump into script directory. Please download manually.')
+                sys.exit(2)
+        logging.info('Windows prerequisite check passed.')
+        return 0
+    else:
+        return 0
+    
 
 # Detect UDP port used by service
-# NOTE: To be extended for Windows, maybe use netsh trace?
 def detect_UDP_port(secs):
     # Probe for UDP packets and count number of packets per port
     timeout = time.time() + secs
     ports = {}
     while time.time() < timeout:
         if platform.system() == 'Darwin' or platform.system() == 'Linux':
-            proc = subprocess.Popen(['sudo', 'tcpdump', '-n', '-c1', 'ip and udp and greater ' + str(minPackLen)], stdout=subprocess.PIPE, stderr=devnull, stdin=subprocess.PIPE)
-            output, err = proc.communicate()
-            logging.debug('Subprocess output returned: %s', output)
-            match = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]*', output)[1].split('.')[4].strip()
-            logging.debug('Match after regexes and split: %s', match)
+            proc = subprocess.Popen(['sudo', 'tcpdump', '-n', '-c1', 'ip and udp and greater ' + str(minPackLen)], stdout=subprocess.PIPE, stderr=NULL_DEVICE, stdin=subprocess.PIPE)
+        if platform.system() == 'Windows':
+            proc = subprocess.Popen([CUR_DIR + '\WinDump.exe', '-n', '-c1', 'ip and udp and greater ' + str(minPackLen)], stdout=subprocess.PIPE, stderr=NULL_DEVICE, stdin=subprocess.PIPE)
+        output, err = proc.communicate()
+        logging.debug('Subprocess output returned: %s', output)
+        match = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]*', output)[1].split('.')[4].strip()
+        logging.debug('Match after regexes and split: %s', match)
         if match in ports:
             ports[match] += 1
         else:
@@ -35,15 +61,16 @@ def detect_UDP_port(secs):
     return port
 
 # Function to extract IP from UDP packet
-# NOTE: To be extended for Windows, maybe use netsh trace?
 def get_IP_from_UDP_packet(port, minPackLen):
     if platform.system() == 'Darwin' or platform.system() == 'Linux':
-        proc = subprocess.Popen(['sudo', 'tcpdump', '-n', '-c1', 'ip and udp src port ' + str(port) + ' and greater ' + str(minPackLen)], stdout=subprocess.PIPE, stderr=devnull, stdin=subprocess.PIPE)
-        output, err = proc.communicate()
-        logging.debug('Subprocess output returned: %s', output)
-        match = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', output)[1].strip()
-        logging.debug('Match after regexes: %s', match)
-        return match
+        proc = subprocess.Popen(['sudo', 'tcpdump', '-n', '-c1', 'ip and udp src port ' + str(port) + ' and greater ' + str(minPackLen)], stdout=subprocess.PIPE, stderr=NULL_DEVICE, stdin=subprocess.PIPE)
+    if platform.system() == 'Windows':
+        proc = subprocess.Popen([CUR_DIR + '\WinDump.exe', '-n', '-c1', 'ip and udp src port ' + str(port) + ' and greater ' + str(minPackLen)], stdout=subprocess.PIPE, stderr=NULL_DEVICE, stdin=subprocess.PIPE)
+    output, err = proc.communicate()
+    logging.debug('Subprocess output returned: %s', output)
+    match = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', output)[1].strip()
+    logging.debug('Match after regexes: %s', match)
+    return match
 
 # Function to get data for IP from geolocation API
 def get_IP_data(ip):
@@ -55,7 +82,8 @@ def get_IP_data(ip):
 
 if __name__ == '__main__':
     print '### REAL-TIME UDP IP GEOLOCATOR ###'
-    print '==================================='
+
+    Windows_prereq_check()
 
     # Request config input
     minPackLen = raw_input('Please enter the minimum packet size which should trigger a frame recording (Default: 200): ')
@@ -66,8 +94,8 @@ if __name__ == '__main__':
     if minPackLen == '':
         minPackLen = 200
     if port == '':
-        raw_input('Please start the service, make sure that it\'s running, turn off the webcam and mic, and press enter to continue. The UDP packets will be probed for 5 seconds, and the most common destination port will be assumed as the one to use.')
-        port = detect_UDP_port(5)
+        raw_input('Please start the service, make sure that it\'s running, turn off the webcam and mic, and press enter to continue. After the port has been detected, re-enable the webcam and mic. The UDP packets will be probed for 5 seconds, and the most common destination port will be assumed as the one to use.')
+        port = detect_UDP_port(int(PORT_PROBING_TIME))
     if timeout == '':
         timeout = 1
 
@@ -83,8 +111,8 @@ if __name__ == '__main__':
                 print 'Capturing UDP packet on port ' + str(port) + '...'
                 print get_IP_data(ip)
                 oldIp = ip
-                time.sleep(float(timeout))
             else:
                 logging.debug('IP %s is identical to old IP', ip)
+            time.sleep(float(timeout))
         except KeyboardInterrupt:
             sys.exit()
